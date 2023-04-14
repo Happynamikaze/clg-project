374f167e6856fce227ee1a6e4ac1e6116bdb4fee
WarClouser <ss4039417@gmail.com>
2023-04-14 14:11:56 +0530

    started file img navbae slider

---
+++ main.js
@@ -0,0 +1,19 @@
+
+// let hamburger = $(".hamburger");
+// let menu = $(".item_right .navigation_bar ul");
+// let menuOverlay = $(".mobile_overlay");
+// let closeMenu = $(".close_menu");
+// hamburger.on("click", () => {
+//     menuToggle();
+// });
+// menuOverlay.on("click", () => {
+//     menuToggle();
+// });
+// closeMenu.on("click", () => {
+//     menuToggle();
+// });
+
+// function menuToggle() {
+//     menuOverlay.toggleClass("active-nav");
+//     menu.toggleClass("active-nav");
+// }
